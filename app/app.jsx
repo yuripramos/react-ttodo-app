e3674fd7fe9bb6d48fee5932cc2d70017377b581
Yuri Ramos <yuri.pramos@gmail.com>
2017-07-14 08:00:32 -0300

    adding routes, downgrade eslint and update .eslintrc rules

--- app/app.jsx
+++ app/app.jsx
@@ -1,21 +1,29 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Provider} = require('react-redux');
-import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+var { Provider } = require('react-redux');
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 var TodoApp = require('TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
+import Login from './components/Login.js';
 
 import './styles/app.css';
 
+const Wildcard = () => <Redirect to="/" />;
 
 store.dispatch(actions.startAddTodos());
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/todos" component={TodoApp} />
+        <Route exact path="/*" render={Wildcard} />
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
---
+++ app/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <p> Login Component Rendering </p>
+  );
+};
+
+export default Login;
